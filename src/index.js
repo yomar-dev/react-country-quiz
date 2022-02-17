047d1db845d336e85984411fed5306c8cf90847e
Yomar Miranda <mirandayomar@gmail.com>
2022-02-16 21:50:59 -0500

    feat: display footer in the app.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.module.scss';
 import App from './App';
+import Footer from 'components/footer/Footer';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
