b584f944259937a1342d9080c2cd3313b19da6b3
Yomar Miranda <mirandayomar@gmail.com>
2022-02-16 21:55:15 -0500

    feat: display game in the app (only UI).

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Game from './components/game/Game';
 import styles from './App.module.scss';
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={styles.container__title}>Country quiz</h1>
+        <Game />
+      </div>
+    </main>
+  );
 }
 
 export default App;
