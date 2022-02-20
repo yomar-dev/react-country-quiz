bb51eab318cf9703d26d24a0c20080025f9f7cdf
Yomar Miranda <mirandayomar@gmail.com>
2022-02-20 17:08:38 -0500

    feat: receive the entire object (country, capital and flags) in option component.

--- src/components/option/Option.js
+++ src/components/option/Option.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import styles from './Option.module.scss';
 
-const Option = ({ letter, value }) => {
+const Option = ({ letter, option }) => {
   return (
     <button className={styles.option}>
       <span className={styles.option__key}>{letter}</span>
-      <span className={styles.option__value}>{value}</span>
+      <span className={styles.option__value}>{option.capital[0]}</span>
     </button>
   );
 };
