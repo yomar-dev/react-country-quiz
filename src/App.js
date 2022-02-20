32a30e88056ee40e5d22d407efd2d21cee00a031
Yomar Miranda <mirandayomar@gmail.com>
2022-02-19 19:07:48 -0500

    feat: get countries.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import { useState, useEffect } from 'react';
+
 import Game from './components/game/Game';
 import styles from './App.module.scss';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((response) => response.json())
+      .then((responseData) => {
+        const transformedCountries = responseData
+          .filter((country) => country.capital)
+          .map((country) => {
+            const { capital, flags, name } = country;
+            return {
+              country: name.common,
+              capital,
+              flags,
+            };
+          });
+        setCountries(transformedCountries);
+      });
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.container__title}>Country quiz</h1>
-        <Game />
+        {countries && <Game countries={countries} />}
       </div>
     </main>
   );
