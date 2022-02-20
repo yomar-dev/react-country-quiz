ece2e10c94ccdfb6d5c701d45d9da35859c4e8fa
Yomar Miranda <mirandayomar@gmail.com>
2022-02-20 18:46:11 -0500

    feat: create result component.

---
+++ src/components/result/Result.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from './Result.module.scss';
+
+import { ReactComponent as WinnersImage } from 'assets/winners.svg';
+
+const Result = ({ score, onResetGame }) => {
+  return (
+    <section className={styles.card}>
+      <picture className={styles.card__image}>
+        <WinnersImage />
+      </picture>
+
+      <h2 className={styles.card__title}>Results</h2>
+      <p className={styles.card__text}>
+        You got <span className={styles.card__score}>{score}</span> correct
+        answers
+      </p>
+
+      <button className={styles.card__button} onClick={onResetGame}>
+        Try again
+      </button>
+    </section>
+  );
+};
+
+export default Result;
