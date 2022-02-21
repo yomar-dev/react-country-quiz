2ed4be2a3a25bd973341866bb0489e44f4717189
Yomar Miranda <mirandayomar@gmail.com>
2022-02-21 09:05:41 -0500

    feat: return the selected option.

--- src/components/option/Option.js
+++ src/components/option/Option.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import styles from './Option.module.scss';
 
-const Option = ({ letter, option }) => {
+const Option = ({ letter, option, onSelectedOption }) => {
   return (
-    <button className={styles.option}>
+    <button className={styles.option} onClick={() => onSelectedOption(option)}>
       <span className={styles.option__key}>{letter}</span>
-      <span className={styles.option__value}>{option.capital[0]}</span>
+      <span className={styles.option__value}>{option.country}</span>
     </button>
   );
 };
