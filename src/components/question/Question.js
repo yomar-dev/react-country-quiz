7cfe3845b4fa4e06ff47a5398ba085a63975acca
Yomar Miranda <mirandayomar@gmail.com>
2022-02-21 07:49:06 -0500

    feat: create question component.

---
+++ src/components/question/Question.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './Question.module.scss';
+
+import Option from 'components/option/Option';
+import { ReactComponent as AdventureImage } from 'assets/adventure.svg';
+
+const LETTERS = ['A', 'B', 'C', 'D'];
+
+const Question = ({ options, answer, onSelectedOption }) => {
+  return (
+    <section className={styles.card}>
+      <h2 className={styles.card__title}>{answer.capital} is the capital of</h2>
+
+      {options.map((option, index) => (
+        <div className={styles.card__option} key={option.country}>
+          <Option
+            letter={LETTERS[index]}
+            option={option}
+            onSelectedOption={onSelectedOption}
+          />
+        </div>
+      ))}
+
+      <picture className={styles.card__image}>
+        <AdventureImage />
+      </picture>
+    </section>
+  );
+};
+
+export default Question;
