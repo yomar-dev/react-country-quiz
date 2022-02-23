082a28acadd3fd0e2123803b26674d742c3bd28e
Yomar Miranda <mirandayomar@gmail.com>
2022-02-23 08:04:10 -0500

    feat: mark correct and incorrect answers properly.

--- src/components/option/Option.js
+++ src/components/option/Option.js
@@ -6,35 +6,30 @@ const Option = ({
   letter,
   option,
   answer,
+  disabled,
   selectedAnswer,
-  answerIsCorrect,
   onSelectedOption,
 }) => {
   let optionClasses = `${styles.option}`;
 
-  if (
-    answer?.country === option.country &&
-    answer?.country === selectedAnswer?.country
-  ) {
-    optionClasses += ` ${styles['option--is-correct']}`;
-  }
+  if (selectedAnswer) {
+    if (answer.country === option.country) {
+      optionClasses += ` ${styles['option--is-correct']}`;
+    }
 
-  if (
-    selectedAnswer?.country === option.country &&
-    selectedAnswer?.country !== answer?.country
-  ) {
-    optionClasses += ` ${styles['option--is-incorrect']}`;
+    if (
+      selectedAnswer.country === option.country &&
+      selectedAnswer.country !== answer.country
+    ) {
+      optionClasses += ` ${styles['option--is-incorrect']}`;
+    }
   }
-
-  const selectOptionHandler = () => {
-    onSelectedOption(option);
-  };
 
   return (
     <button
       className={optionClasses}
-      onClick={selectOptionHandler}
-      disabled={answerIsCorrect}
+      onClick={() => onSelectedOption(option)}
+      disabled={disabled}
     >
       <span className={styles.option__key}>{letter}</span>
       <span className={styles.option__value}>{option.country}</span>
