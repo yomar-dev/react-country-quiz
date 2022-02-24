b87e9afd8c051324f6c5e5dccd7c55b4722c037c
Yomar Miranda <mirandayomar@gmail.com>
2022-02-23 20:23:33 -0500

    perf: remove unnecessary code.

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import styles from './Game.module.scss';
-
 import Result from 'components/result/Result';
 import Question from 'components/question/Question';
 
